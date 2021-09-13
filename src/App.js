69fef913d4d71ac0ccc0eda4c24163f33f79d0bb
Eperjesi Attila Dávid <eper.atis@gmail.com>
2021-09-13 22:11:00 +0200

    added routing and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import './App.scss';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
+//pages
 import StudentsList from './component/StudentsList';
 import StaffList from './component/StaffList';
 import Homepage from './component/Homepage';
+import Navigation from './component/Navigaton';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-      <StudentsList />
-      <StaffList />
-    </div>
+    <Router>
+      <div>
+        <div class="row">
+          <Navigation />
+        </div>
+        <div class="row">
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
+            <Route exact path="/home" component={Homepage} />
+            <Route exact path="/students" component={StudentsList} />
+            <Route exact path="/staff" component={StaffList} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -9,10 +9,10 @@ class Homepage extends React.Component {
                     <Col>Thanks for wisiting my little project!</Col>
                 </Row>
                 <Row>
-                    <Col>It's created with ReactJS and the <a class="btn-outline-primary" href="https://hp-api.herokuapp.com/" target="_blank"><i>back-end</i></a> part is already created by someone.</Col>
+                    <Col>It's created with ReactJS and the <a class="btn-outline-primary" rel="noreferrer" href="https://hp-api.herokuapp.com/" target="_blank"><i>back-end</i></a> part is already created by someone.</Col>
                 </Row>
                 <Row>
-                    <Col>If you are here to check the code, you can see <a class="btn-outline-primary" href="https://github.com/eperatis/harry-potter-reactjs" target="_blank"><i>here</i></a>, if you intrested you can check my other <a class="btn-outline-primary" href="https://github.com/eperatis" target="_blank"><i>github</i></a> projects.</Col>
+                    <Col>If you are here to check the code, you can see <a class="btn-outline-primary" rel="noreferrer" href="https://github.com/eperatis/harry-potter-reactjs" target="_blank"><i>here</i></a>, if you intrested you can check my other <a class="btn-outline-primary" rel="noreferrer" href="https://github.com/eperatis" target="_blank"><i>github</i></a> projects.</Col>
                 </Row>
             </Container>
         );
